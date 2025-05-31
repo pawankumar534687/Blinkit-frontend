08b42128bbd6a0df15610fd9c984dd1b02c8efaf
pawankumar534687 <pawankumar534687@gmail.com>
2025-05-31 10:34:23 -1200

    update hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
 import ScrollToTop from './components/navbar/ScrollToTop'
  import { ToastContainer } from 'react-toastify';
-
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
  
   <StrictMode>
-    <BrowserRouter>
+   <HashRouter>
     <ScrollToTop/>
     <App />
       <ToastContainer />
-    </BrowserRouter>
+   </HashRouter>
   </StrictMode>
  
 )
